17e4b5a5bfa09b4f3d03ab12d03ab840bb7dea9e
Sidnei Cerqueira <sidnei.c.rodrigues@hotmail.com>
2022-10-16 11:32:23 -0300

    adicionando itens ao carrinho

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 import {pizzaJson} from '../products/pizzas.js'
 
+// Variavel de itens no carrinho
+let cart = [];
+// Variavel com a quantidade de pizza
+let modalQtdPizza = 0;
+let modalKey = 0;
+
 //funcòes que selecionam os elementos do HTML
 const selectElement = (el) => document.querySelector(el);
-const SelectAll = (el) => document.querySelectorAll(el);
+const selectAll = (el) => document.querySelectorAll(el);
 
 // Mapeamento de cada item do array produtos pizzas.js
 pizzaJson.map((item, index)=> {
@@ -25,19 +31,28 @@ pizzaJson.map((item, index)=> {
 
         // recuperando e dentificando o elemento clicado para exibicão no modal
          /* closest() procura o elemento mais proximo e que tem o paramentro epecificado*/
-        let itemKey = e.target.closest('.pizza-item').getAttribute('data-key');            
-        console.log(pizzaJson[itemKey]);
+        let itemKey = e.target.closest('.pizza-item').getAttribute('data-key');  
+        modalQtdPizza = 1;
+        modalKey = itemKey;
 
         // Populando o modal com as informações do item clicado
         selectElement('.pizzaBig img').src = pizzaJson[itemKey].img;
         selectElement('.pizzaInfo h1').innerHTML = pizzaJson[itemKey].name;
         selectElement('.pizzaInfo--desc').innerHTML = pizzaJson[itemKey].description;
-        selectElement('.pizzaInfo--actualPrice').innerHTML =  `R$ ${pizzaJson[itemKey].price.toFixed(2)}`;
-        
-        // ?Inserindo os tamanhos e quantidade de cada pizza
-        SelectAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+        selectElement('.pizzaInfo--actualPrice').innerHTML = exibirPrice(2).toFixed(2);        
+
+        //Tirando a seleçao padrao do tamanho das pizzas
+        selectElement('.pizzaInfo--size.selected').classList.remove('selected')
+
+        // Inserindo os tamanhos e quantidade de cada pizza
+        selectAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2){
+                size.classList.add('selected')
+            }
             size.querySelector('span').innerHTML = pizzaJson[itemKey].sizes[sizeIndex];
-        })
+        });
+
+        selectElement('.pizzaInfo--qt').innerHTML = modalQtdPizza;
 
 
         //Exebindo o modal com animação
@@ -45,10 +60,88 @@ pizzaJson.map((item, index)=> {
         selectElement('.pizzaWindowArea').style.display = 'flex';
         setTimeout(()=>{
             selectElement('.pizzaWindowArea').style.opacity = 1;
-        }, 300)
+        }, 300);
    })
 
 
    // Adcionar os itens de cada pizza na tela
    selectElement('.pizza-area').appendChild(pizzaItem)  ; 
-})
+});
+
+
+function exibirPrice(size) {
+    let price = pizzaJson[modalKey].price
+
+    if(size == 0){
+       price = 22.90;
+       return pizzaJson[modalKey].price = price;
+    }
+    else if(size == 1){
+       price = 35.00;
+       return pizzaJson[modalKey].price = price;
+    }
+    else if(size == 2){
+      price = 47.90;
+      return pizzaJson[modalKey].price = price;
+    }   
+}
+
+
+// Eventos do modal
+function closeModal() {
+    selectElement('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        selectElement('.pizzaWindowArea').style.display = 'none';
+    }, 500);    
+}
+
+
+window.document.addEventListener('click', function(e){
+    const el = e.target;
+    if(el.classList.contains('pizzaInfo--cancelButton') || el.classList.contains('pizzaInfo--cancelMobileButton')){
+        closeModal();
+    }
+    else if(el.classList.contains('pizzaInfo--qtmenos')){    
+        if(modalQtdPizza > 1){
+            modalQtdPizza--;
+            selectElement('.pizzaInfo--qt').innerHTML = modalQtdPizza;
+        }
+    }
+    else if(el.classList.contains('pizzaInfo--qtmais')){        
+       modalQtdPizza++;
+       selectElement('.pizzaInfo--qt').innerHTML = modalQtdPizza;        
+    }
+    else if(el.classList.contains('pizzaInfo--addButton')){
+        //Qual a pizza?
+        console.log("pizza: "+modalKey)
+        //qual o tamanho?
+        let size = parseInt(selectElement('.pizzaInfo--size.selected').getAttribute('data-key'))
+        console.log("Tamanho: "+size)
+        //qual a quantidade
+        console.log("Quantidade: "+modalQtdPizza)
+
+        cart.push({
+            id: pizzaJson[modalKey].id,
+            size:size,
+            qt: modalQtdPizza
+
+        })
+
+        console.log(cart)
+        closeModal();
+    }   
+});
+
+selectAll('.pizzaInfo--size').forEach((size)=> {   
+    size.addEventListener('click', ()=>{
+        selectElement('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+
+        let sizeItem = selectElement('.pizzaInfo--size.selected').getAttribute('data-key')
+        selectElement('.pizzaInfo--actualPrice').innerHTML = (exibirPrice(sizeItem).toFixed(2))
+    })                             
+});
+
+console.log(navigator.appName)
+console.log(browser.name)
+
--- assets/products/pizzas.js
+++ assets/products/pizzas.js
@@ -3,7 +3,7 @@ export let pizzaJson = [
     {id:2, name:'Calabresa', img:'./assets/images/pizza2.png', price:18.00, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:3, name:'Quatro Queijos', img:'./assets/images/pizza3.png', price:17.45, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:4, name:'Americana', img:'./assets/images/pizza4.png', price:19.77, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'./assets/images/pizza5.png', price:21.43, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Marguerita', img:'./assets/images/pizza5.png', price:21.43, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:6, name:'Moda da Casa', img:'./assets/images/pizza6.png', price:18.55, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:7, name:'Chocolate', img:'./assets/images/pizza7.png', price:22.36, sizes:['4 Fatias', '8 Fatias', '12 Fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
